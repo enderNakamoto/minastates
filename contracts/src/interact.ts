c70fb2d9d540db858819f4d2857634ae47ce62f4
enderNakamoto <ender.nakamoto@gmail.com>
2024-08-10 19:52:31 -0400

    experimentation

--- contracts/src/interact.ts
+++ contracts/src/interact.ts
@@ -1,112 +1,67 @@
-/**
- * This script can be used to interact with the Add contract, after deploying it.
- *
- * We call the update() method on the contract, create a proof and send it to the chain.
- * The endpoint that we interact with is read from your config.json.
- *
- * This simulates a user interacting with the zkApp from a browser, except that here, sending the transaction happens
- * from the script and we're using your pre-funded zkApp account to pay the transaction fee. In a real web app, the user's wallet
- * would send the transaction and pay the fee.
- *
- * To run locally:
- * Build the project: `$ npm run build`
- * Run with node:     `$ node build/src/interact.js <deployAlias>`.
- */
-import fs from 'fs/promises';
-import { Mina, NetworkId, PrivateKey } from 'o1js';
-import { Add } from './Add.js';
+import { Square } from './Square.js';
+import { Field, Mina, PrivateKey, AccountUpdate } from 'o1js';
 
-// check command line arg
-let deployAlias = process.argv[2];
-if (!deployAlias)
-  throw Error(`Missing <deployAlias> argument.
+const useProof = false;
 
-Usage:
-node build/src/interact.js <deployAlias>
-`);
-Error.stackTraceLimit = 1000;
-const DEFAULT_NETWORK_ID = 'testnet';
+const Local = await Mina.LocalBlockchain({ proofsEnabled: useProof });
+Mina.setActiveInstance(Local);
 
-// parse config and private key from file
-type Config = {
-  deployAliases: Record<
-    string,
-    {
-      networkId?: string;
-      url: string;
-      keyPath: string;
-      fee: string;
-      feepayerKeyPath: string;
-      feepayerAlias: string;
-    }
-  >;
-};
-let configJson: Config = JSON.parse(await fs.readFile('config.json', 'utf8'));
-let config = configJson.deployAliases[deployAlias];
-let feepayerKeysBase58: { privateKey: string; publicKey: string } = JSON.parse(
-  await fs.readFile(config.feepayerKeyPath, 'utf8')
-);
+const deployerAccount = Local.testAccounts[0];
+const deployerKey = deployerAccount.key;
+const senderAccount = Local.testAccounts[1];
+const senderKey = senderAccount.key;
+// ----------------------------------------------------
 
-let zkAppKeysBase58: { privateKey: string; publicKey: string } = JSON.parse(
-  await fs.readFile(config.keyPath, 'utf8')
-);
+// Create a public/private key pair. The public key is your address and where you deploy the zkApp to
+const zkAppPrivateKey = PrivateKey.random();
+const zkAppAddress = zkAppPrivateKey.toPublicKey();
 
-let feepayerKey = PrivateKey.fromBase58(feepayerKeysBase58.privateKey);
-let zkAppKey = PrivateKey.fromBase58(zkAppKeysBase58.privateKey);
-
-// set up Mina instance and contract we interact with
-const Network = Mina.Network({
-  // We need to default to the testnet networkId if none is specified for this deploy alias in config.json
-  // This is to ensure the backward compatibility.
-  networkId: (config.networkId ?? DEFAULT_NETWORK_ID) as NetworkId,
-  mina: config.url,
+// create an instance of Square - and deploy it to zkAppAddress
+const zkAppInstance = new Square(zkAppAddress);
+const deployTxn = await Mina.transaction(deployerAccount, async () => {
+  AccountUpdate.fundNewAccount(deployerAccount);
+  await zkAppInstance.deploy();
 });
-// const Network = Mina.Network(config.url);
-const fee = Number(config.fee) * 1e9; // in nanomina (1 billion = 1.0 mina)
-Mina.setActiveInstance(Network);
-let feepayerAddress = feepayerKey.toPublicKey();
-let zkAppAddress = zkAppKey.toPublicKey();
-let zkApp = new Add(zkAppAddress);
+await deployTxn.sign([deployerKey, zkAppPrivateKey]).send();
 
-// compile the contract to create prover keys
-console.log('compile the contract...');
-await Add.compile();
+// get the initial state of Square after deployment
+const num0 = zkAppInstance.num.get();
+console.log('state after init:', num0.toString());
+
+// ----------------------------------------------------
+
+const txn1 = await Mina.transaction(senderAccount, async () => {
+  await zkAppInstance.update(Field(9));
+});
+await txn1.prove();
+await txn1.sign([senderKey]).send();
+
+const num1 = zkAppInstance.num.get();
+console.log('state after txn1:', num1.toString());
+
+// ----------------------------------------------------
 
 try {
-  // call update() and send transaction
-  console.log('build transaction and create proof...');
-  let tx = await Mina.transaction(
-    { sender: feepayerAddress, fee },
-    async () => {
-      await zkApp.update();
-    }
-  );
-  await tx.prove();
-
-  console.log('send transaction...');
-  const sentTx = await tx.sign([feepayerKey]).send();
-  if (sentTx.status === 'pending') {
-    console.log(
-      '\nSuccess! Update transaction sent.\n' +
-        '\nYour smart contract state will be updated' +
-        '\nas soon as the transaction is included in a block:' +
-        `\n${getTxnUrl(config.url, sentTx.hash)}`
-    );
-  }
-} catch (err) {
-  console.log(err);
+  const txn2 = await Mina.transaction(senderAccount, async () => {
+    await zkAppInstance.update(Field(75));
+  });
+  await txn2.prove();
+  await txn2.sign([senderKey]).send();
+} catch (error: any) {
+  console.log(error.message);
 }
+const num2 = zkAppInstance.num.get();
+console.log('state after txn2:', num2.toString());
 
-function getTxnUrl(graphQlUrl: string, txnHash: string | undefined) {
-  const hostName = new URL(graphQlUrl).hostname;
-  const txnBroadcastServiceName = hostName
-    .split('.')
-    .filter((item) => item === 'minascan')?.[0];
-  const networkName = graphQlUrl
-    .split('/')
-    .filter((item) => item === 'mainnet' || item === 'devnet')?.[0];
-  if (txnBroadcastServiceName && networkName) {
-    return `https://minascan.io/${networkName}/tx/${txnHash}?type=zk-tx`;
-  }
-  return `Transaction hash: ${txnHash}`;
-}
+// ----------------------------------------------------
+
+const txn3 = await Mina.transaction(senderAccount, async () => {
+  await zkAppInstance.update(Field(81));
+});
+await txn3.prove();
+await txn3.sign([senderKey]).send();
+
+const num3 = zkAppInstance.num.get();
+console.log('state after txn3:', num3.toString());
+
+// ----------------------------------------------------
